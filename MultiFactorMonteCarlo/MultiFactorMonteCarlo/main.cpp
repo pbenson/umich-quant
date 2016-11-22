14ed9cb61b637c838ef584d88ff24bc13450afc3
hanxiye <hanxiye@0587336811.wireless.umich.net>
2016-11-22 14:51:46 -0500

    Added VaR and ES

--- MultiFactorMonteCarlo/MultiFactorMonteCarlo/main.cpp
+++ MultiFactorMonteCarlo/MultiFactorMonteCarlo/main.cpp
@@ -20,6 +20,23 @@
 #include "MarketScenario.hpp"
 #include "MarketSimulation.hpp"
 
+#include <algorithm>
+
+double var(std::vector<double> profits, double confidence){
+    std::sort(profits.begin(),profits.end());
+    size_t varIndex = (int)(profits.size() + 1) * (1 - confidence) - 1;
+    return -profits[varIndex];
+}
+
+double es(std::vector<double> profits, double confidence){
+    std::sort(profits.begin(),profits.end());
+    size_t varIndex = (int)(profits.size() + 1) * (1 - confidence) - 1;
+    double expectedShortfall = 0;
+    for(int i = 0; i < varIndex+1; ++i){
+        expectedShortfall += profits[i];
+    }
+    return -expectedShortfall / (varIndex + 1);
+}
 int main(int argc, const char * argv[]) {
     std::ifstream inputPrices("IBM_T_NKE_AAPL_20141001-20160930.csv");
     if (!inputPrices.is_open()) {
@@ -46,5 +63,12 @@ int main(int argc, const char * argv[]) {
     const MarketScenario scenario(market, 2016, 9, 30);
     std::cout << portfolio.value(scenario) << std::endl;
     std::cout << portfolio.value(scenario,simulation1) << std::endl;
+    
+    size_t numSims = 9999;
+    std::vector<double> result = portfolio.profits(scenario,numberHistoricalReturns, int(numSims));
+    
+    std::cout << "95% VaR = " << var(result,0.95)<<std::endl;
+    
+    std::cout << "Expected Shortfall = " << es(result,0.95) << std::endl;
     return 0;
 }
